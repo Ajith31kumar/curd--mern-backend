deddff9198f1e3640cf680b296d4fe8e4f5ba92a
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2024-04-18 12:58:15 +0530

    first commit

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const router = require('./routes')
 
-//using cors for the cross origin
-//using cookieparser to store tokens in user's cookie section
+// Using cors for cross-origin requests
+// Using cookie-parser to handle cookies
 
 const app = express()
 
 mongoose.connect(process.env.MONGO)
 .then(() => {
+    // Define route for the root endpoint
+    app.get("/", (req, res) => {
+        res.status(200).send(`<h1>Welcome to task management app</h1>`);
+    });
+    
     console.log('Database Connected')
+
+    // Set up middleware
     app.use(express.json())
     app.use(cors({
         origin: 'http://localhost:3000',
@@ -21,22 +28,12 @@ mongoose.connect(process.env.MONGO)
         credentials: true
     }));
     app.use(cookieParser())
+
+    // Mount the router for API endpoints
     app.use('/api', router)
-    app.listen(5000, () => console.log('server is running on port 5000'))
+
+    // Start the server
+    app.listen(5000, () => console.log('Server is running on port 5000'))
 
 })
-// .catch((err) => console.error(err) )
-// mongoose.connect(process.env.MONGO)
-// .then(() => {
-//     console.log('Database Connected')
-//     app.use(express.json())
-//     app.use(cors({
-//         origin: '*',
-//         methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-//         credentials: true
-//     }));
-//     app.use(cookieParser())
-//     app.use('/api', router)
-//     app.listen(5000, () => console.log('server is running on port 5000'))
-// })
-// .catch((err) => console.error(err) )
+.catch((err) => console.error(err))
